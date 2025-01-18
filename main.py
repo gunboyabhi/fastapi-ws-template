50df03d4fc13fd6d644e0f012ce6b0bfcfe51660
Abhi Gunjal <59883542+gunboyabhi@users.noreply.github.com>
2025-01-19 02:54:13 +0530

    changed code

--- main.py
+++ main.py
@@ -3,13 +3,20 @@ import httpx  # Use httpx for async HTTP requests
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
+import requests
+import base64
+import json
+import uuid
 
 load_dotenv()
 
 app = FastAPI()
 
+
+ACCESS_KEY = os.environ.get("ACCESS_KEY")
+DB_KEY = os.environ.get("DB_KEY")
 origins = [
-    "http://localhost:3000",
+    "*",
 ]
 
 app.add_middleware(
@@ -69,11 +76,84 @@ async def run_flow_via_http(message: str):
         return {"error": f"An error occurred: {str(e)}"}
 
 
+def get_lat_lon(city, state, api_key):
+    url = f"https://maps.googleapis.com/maps/api/geocode/json"
+    params = {"address": f"{city}, {state}", "key": api_key}
+    response = requests.get(url, params=params).json()
+    if response['status'] == 'OK':
+        location = response['results'][0]['geometry']['location']
+        return {"lat": location['lat'], "lon": location['lng']}
+    return {"error": "Invalid location"}
+
+
+
+def get_data(request):
+
+    url = "https://json.freeastrologyapi.com/planets"
+
+    payload = json.dumps({
+    "name": request.name,
+    "year": request.year,
+    "month": request.month,
+    "date": request.date,
+    "hours": request.hours,
+    "minutes": request.minutes,
+    "seconds": request.seconds,
+    "latitude": 90,
+    "longitude": 180,
+    "timezone": 5.5,
+    "settings": {
+        "observation_point": "topocentric",
+        "ayanamsha": "lahiri"
+    }
+    })
+
+    headers = {
+    'Content-Type': 'application/json',
+    'x-api-key': ACCESS_KEY
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    if response:
+        json_res = json.loads(response.text)
+        print(json_res)
+        return json_res
+
+
+def insert_data_into_astra_db(data):
+    from astrapy import DataAPIClient
+
+    # Initialize the client
+    client = DataAPIClient(DB_KEY)
+    db = client.get_database_by_api_endpoint(
+    "https://299ee1d3-7813-4d96-8ea6-9684f8b9b124-us-east-2.apps.astra.datastax.com"
+    )
+
+    keyspace = "default_keyspace" 
+    collection_name = "stored_responses"
+    collection = db.get_collection(collection_name)
+    collection.insert_one(data)
+    print("Data inserted successfully.")
+
 from pydantic import BaseModel
 
-class MessageRequest(BaseModel):
-    message: str
+class chatRequest(BaseModel):
+    name: str
+    day: int
+    month: int
+    date: int
+    year: int
+    hours: int
+    minutes: int
+    seconds: int
+    gender: str
+    state: str
+    city: str
 
 @app.post("/process")
-async def process_message(request: MessageRequest):
-    return await run_flow_via_http(request.message)
+def process_message(request: chatRequest):
+    # get token
+    data = get_data(request)
+    insert_data_into_astra_db(data=data)
+    return {"message": "successfully submitted", 'status': 200}
